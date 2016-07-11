1719f7bd3453be99d4bee608ea1e7ba42e68f681
martinzapletal <zapletal-martin@email.cz>
2016-07-10 23:53:19 -0400

    Script

--- run-example.sh
+++ run-example.sh
@@ -2,9 +2,9 @@
 
 cd output/target
 
-unzip gearpump-2.11-0.8.1-RC0.zip
+unzip gearpump-2.11-0.8.1-RC3.zip
 
-cd gearpump-2.11-0.8.1-RC0/bin
+cd gearpump-2.11-0.8.1-RC3/bin
 
 chmod +x services
 chmod +x gear
@@ -13,4 +13,4 @@ chmod +x local
 ./services &
 ./local &
 
-./gear app -jar ../../../../examples/target/2.11/cassandra-2.11-0.8.1-RC0-assembly.jar org.apache.gearpump.streaming.examples.cassandra.CassandraTransform
+./gear app -jar ../../../../examples/target/2.11/cassandra-2.11-0.8.1-RC3-assembly.jar org.apache.gearpump.streaming.examples.cassandra.CassandraTransform
