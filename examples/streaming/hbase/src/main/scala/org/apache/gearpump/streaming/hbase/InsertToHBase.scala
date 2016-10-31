0e1df27b4e78797ced14690980c9066189af7afc
Roshanson <736781877@qq.com>
2016-10-31 13:12:32 +0800

    Add example of connecting to HBase

--- examples/streaming/hbase/src/main/scala/org/apache/gearpump/streaming/hbase/InsertToHBase.scala
+++ examples/streaming/hbase/src/main/scala/org/apache/gearpump/streaming/hbase/InsertToHBase.scala
@@ -3,7 +3,7 @@
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
+ * regarding copyright ownership.  The ASF licenses this fileenv
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
@@ -37,7 +37,6 @@ class InsertToHBase extends DataSource {
   override def open(context: TaskContext, startTime: Instant): Unit = {}
 
   override def read(): Message = {
-
     hBaseSink.insert(Bytes.toBytes(s"row$x"), Bytes.toBytes("group"),
       Bytes.toBytes("group:name"), Bytes.toBytes("300000"))
       x += 1
